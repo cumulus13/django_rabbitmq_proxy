d9fcca27514dce58a3bbcce93ef9f26ca6f89ebd
cumulus13 <cumulus13@gmail.com>
2023-05-08 11:48:50 +0700

    version: 0.12 ~ 2023-05-08 11:48:47:438411

--- server.py
+++ server.py
@@ -2,8 +2,12 @@ import asyncio
 import json
 import aioamqp
 import ast
+from jsoncolor import jprint
+from  make_colors import make_colors
+from unidecode import unidecode
+import traceback
 
-async def send_to_rabbitmq(message):
+async def send_to_rabbitmq(message, severity = 'DEBUG'):
     try:
         transport, protocol = await aioamqp.connect(
             host='localhost', port=5672,
@@ -12,9 +16,42 @@ async def send_to_rabbitmq(message):
         )
 
         channel = await protocol.channel()
-        await channel.exchange_declare(
-            exchange_name='django', type_name='fanout', durable=True
-        )
+        if severity == 'DEBUG' or severity == 7 or severity == '7':
+            await channel.exchange_declare(
+                exchange_name='django', type_name='fanout', durable=True
+            )
+        elif severity == 'INFO' or severity == 6 or severity == '6':
+            await channel.exchange_declare(
+                exchange_name='django_info', type_name='fanout', durable=True
+            )
+        elif severity == 'NOTICE' or severity == 5 or severity == '5':
+            await channel.exchange_declare(
+                exchange_name='django_notice', type_name='fanout', durable=True
+            )
+        elif severity == 'WARNING' or severity == 4 or severity == '4':
+            await channel.exchange_declare(
+                exchange_name='django_warning', type_name='fanout', durable=True
+            )
+        elif severity == 'ERROR' or severity == 3 or severity == '3':
+            await channel.exchange_declare(
+                exchange_name='django_error', type_name='fanout', durable=True
+            )
+        elif severity == 'CRITICAL' or severity == 2 or severity == '2':
+            await channel.exchange_declare(
+                exchange_name='django_critical', type_name='fanout', durable=True
+            )
+        elif severity == 'ALERT' or severity == 1 or severity == '1':
+            await channel.exchange_declare(
+                exchange_name='django_alert', type_name='fanout', durable=True
+            )
+        elif severity == 'EMERGENCY' or severity == 0 or severity == '' or severity == 'EMERG':
+            await channel.exchange_declare(
+                exchange_name='django_emergency', type_name='fanout', durable=True
+            )
+        else:
+            await channel.exchange_declare(
+                exchange_name='django', type_name='fanout', durable=True
+            )
 
         message = message.encode('utf-8')
         await channel.basic_publish(
@@ -36,16 +73,36 @@ class MyProtocol:
         self.transport = transport
 
     def datagram_received(self, data, addr):
-        message = data.decode('utf-8')
-        print(f"Received message: {message}")
+        try:
+            message = data.decode('utf-8')
+        except UnicodeDecodeError:
+            try:
+                message = unidecode(data)
+            except:
+                print(traceback.format_exc())
+                message = data
+        #print(f"Received message: {message}")
+        error = False
         try:
             json_data = json.loads(message)
+            jprint(json_data)
         except:
             try:
                 json_data = ast.literal_eval(message)
+                print(make_colors(json_data, 'b', 'lg'))
             except:
                 json_data = message
-        asyncio.ensure_future(send_to_rabbitmq(message))
+                print(make_colors(json_data, 'lw', 'r'))
+                error = True
+        if not error:
+            if not isinstance(json_data, str) and json_data.get('levelname') == 'DEBUG':
+                thd = [send_to_rabbitmq(json_data, json_data.get('levelname')), send_to_rabbitmq(json_data)]
+                tasks = [asyncio.ensure_future(coro) for coro in thd]
+                asyncio.gather(*tasks)
+            else:
+                asyncio.ensure_future(send_to_rabbitmq(json_data))
+        else:
+            asyncio.ensure_future(send_to_rabbitmq(json_data))
 
     def error_received(self, exc):
         print('Error received:', exc)
@@ -56,16 +113,28 @@ class MyProtocol:
 
 async def start_udp_server():
     loop = asyncio.get_running_loop()
+    print(
+        make_colors("server listen on", 'lg') + " " + \
+        make_colors("0.0.0.0", 'b', 'ly') + ":" + \
+        make_colors("520", 'b', 'lc')
+    )
     transport, protocol = await loop.create_datagram_endpoint(
         lambda: MyProtocol(loop),
         local_addr=('0.0.0.0', 520)
     )
 
+    #try:
+        #await asyncio.sleep(36000)  # Serve for 1 hour.
+    #finally:
+        #transport.close()
+        #await transport.wait_closed()
     try:
-        await asyncio.sleep(3600)  # Serve for 1 hour.
+        while True:
+            await asyncio.sleep(3600)  # Sleep for 1 second.
     finally:
         transport.close()
         await transport.wait_closed()
+
 
 async def main():
     await start_udp_server()
